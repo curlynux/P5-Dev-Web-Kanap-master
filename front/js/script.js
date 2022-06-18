1a8503dac6d9158af6f5d8ae33e5002eb83d944c
curlynux <curlynux@gmail.com>
2022-06-18 19:31:58 +0200

    name and description work

--- front/js/script.js
+++ front/js/script.js
@@ -2,40 +2,33 @@ var section = document.getElementById("items");
 var link = document.getElementsByClassName("items").items.childNodes[1];
 var id = document.getElementById("");
 var data = (url) => fetch(url)
-.then((res) => res.json())
+.then((response) => response.json())
 .then((data) => 
 {
-    var j = 0;
-    while(j <= 8)
+    var i = 1;
+    while(i <= 8)
     {
-        console.log(data[j++]);
+        var a = document.createElement("a");
+        var article = document.createElement("article");
+        var img = document.createElement("img");
+        var h3 = document.createElement("h3");
+        var p = document.createElement("p");
+    
+        section.appendChild(a);
+        a.setAttribute("href", "./product.html");
+        img.setAttribute("src", "http://localhost:3000/images/kanap0"  + i + ".jpeg");
+        h3.classList.add("productName");
+        p.classList.add("productDescription");
+        h3.innerText = data[i].name;
+        p.innerText = data[i].description;
+        a.appendChild(article);
+        article.appendChild(img);
+        article.appendChild(h3);
+        article.appendChild(p);
+        section.appendChild(a);
+        console.log(data[i++]);
     }
 });
 
-
 data("http://localhost:3000/api/products");
-
-var i = 0;
-while (i <= 7)
-{
-    i++;
-    var a = document.createElement("a");
-    var article = document.createElement("article");
-    var img = document.createElement("img");
-    var h3 = document.createElement("h3");
-    var p = document.createElement("p");
-
-    section.appendChild(a);
-    a.setAttribute("href", "./product.html");
-    img.setAttribute("src", "http://localhost:3000/images/kanap0"  + i + ".jpeg");
-    h3.classList.add("productName");
-    h3.innerText = "text";
-    p.classList.add("productDescription");
-    p.innerText = "text";
-    a.appendChild(article);
-    article.appendChild(img);
-    article.appendChild(h3);
-    article.appendChild(p);
-    section.appendChild(a);
-}
 
