9a5fd6c5388490464f3ee22188c7599f9c523c65
curlynux <curlynux@gmail.com>
2022-06-19 13:14:31 +0200

    not working

--- back/routes/product.js
+++ back/routes/product.js
@@ -1,10 +1,12 @@
-import express from "express";
+const express = require("express");
 const router = express.Router();
 const productCtrl = require('../controllers/product');
+const data = require("../../front/js/script");
 
 router.get('/', productCtrl.getAllProducts);
 router.get('/:id', productCtrl.getOneProduct);
 router.post('/order', productCtrl.orderProducts);
 
-console.log(30)
+console.log(30);
+console.log(data);
 module.exports = router;
