518bc3b243c9703f08bf05017f1c9f0443668712
curlynux <curlynux@gmail.com>
2022-06-22 21:23:26 +0200

    remove placeholder article

--- front/js/product.js
+++ front/js/product.js
@@ -1,1 +1,7 @@
+var data = localStorage.getItem("data");
+
+var img = document.getElementsByTagName("img");
+
+
+console.log(img[5].src);
 console.log(window.location.search);
--- front/js/script.js
+++ front/js/script.js
@@ -1,19 +1,20 @@
 var section = document.getElementById("items");
 var link = document.getElementsByClassName("items").items.childNodes[1];
-var id = document.getElementById("");
-var data = (url) => fetch(url)
-.then((response) => response.json())
-.then((data) => 
+
+var kanap = async (url) =>
 {
+    var response = await fetch(url);
+    var data = await response.json();
+
     var i = 0;
-    while(i < data.length, i++)
+    while(i < data.length)
     {
         var a = document.createElement("a");
         var article = document.createElement("article");
         var img = document.createElement("img");
         var h3 = document.createElement("h3");
         var p = document.createElement("p");
-    
+
         a.setAttribute("href", `./product.html?id=${data[i]._id}`)
         article.id = data[i]._id;
         img.setAttribute("src", data[i].imageUrl);
@@ -30,10 +31,12 @@ var data = (url) => fetch(url)
         article.appendChild(h3);
         article.appendChild(p);
         section.appendChild(a);
-        a.addEventListener("click", () => 
-            document.location.href = a.href);
-            
+        a.addEventListener("click", () => {
+            document.location.href = a.href;
+        });
+       localStorage.setItem("id", JSON.stringify(data[i]._id));
+        console.log(data[i].name);
+        i++;
     }
-});
-
-data("http://localhost:3000/api/products");
+}
+kanap("http://localhost:3000/api/products")
