6401fd09476b6d0078f3e2dc4b56c2468323b499
curlynux <curlynux@gmail.com>
2022-06-23 18:36:09 +0200

    undefined (reading 'imageUrl')

--- front/js/product.js
+++ front/js/product.js
@@ -4,17 +4,20 @@ var img = document.getElementsByTagName("img");
 var h1  = document.getElementById("title");
 var p   = document.getElementById("price");
 var des = document.getElementById("description");
+var title = document.getElementsByTagName("title");
 
-img.src = data[0].imageUrl;
+
 p.textContent = data[0].price;
 h1.textContent = data[0].name;
 des.textContent = data[0].description;
+title.textContent = data[0].name
+console.log("title");
 var i = 0;
 while(i < data.length)
 {
     i++;
 
-    console.log(data[i]);
+    console.log(data[i].imageUrl);
     console.log(img[5]);
     console.log(h1);
     console.log(p);
