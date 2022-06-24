64d2cdeea72a0212444e4f9787bfc901638d6311
curlynux <curlynux@gmail.com>
2022-06-25 00:51:34 +0200

    page produit finit

--- front/js/product.js
+++ front/js/product.js
@@ -7,21 +7,26 @@ var des = document.getElementById("description");
 var title = document.getElementsByTagName("title");
 var url = window.location.href;
 var id = url.substring(url.indexOf("=") + 1);
-
-
+var colors = document.getElementById("colors");
+var option = document.createElement("option");
 
 var i = 0;
 while(i < data.length)
 {
-    i++;
     if(id === data[i]._id)
     {
         p.textContent = data[i].price;
         h1.textContent = data[i].name;
         des.textContent = data[i].description;
         title[0].innerHTML = data[i].name;
-        img[5].src = data[i].imageUrl
-        console.log(data[i])
+        img[5].src = data[i].imageUrl;
+        img[5].alt = data[i].altTxt;
+        
+        console.log(colors);
+        option.value = data[i].colors[2];
+        colors.appendChild(option);
+        colors.children[1].innerHTML = data[i].colors[1]
+        console.log(data[i]);
     }
-    console.log(title);
+    ++i;
 }
