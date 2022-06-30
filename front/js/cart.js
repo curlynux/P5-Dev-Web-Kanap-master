52600482d91668be531da5c13ca933c277442bd8
curlynux <curlynux@gmail.com>
2022-06-30 14:38:51 +0200

    color and quantity works

--- front/js/cart.js
+++ front/js/cart.js
@@ -4,10 +4,13 @@ var item = JSON.parse(localStorage.getItem("item"));
 var h2 = document.getElementsByTagName("h2")[0];
 var color = document.getElementsByTagName("p")[0];
 var prix = document.getElementsByTagName("p")[1];
+var cart = JSON.parse(localStorage.getItem("cart"));
+var color = JSON.parse(localStorage.getItem("color"));
+var quant = localStorage.
+
 
 img[5].src = item.imageUrl;
 h2.innerHTML = item.name;
 color.innerHTML = item.colors[0];
 prix.innerHTML = item.price + " €";
-console.log(item);
-console.log(data);
+console.log(cart);
