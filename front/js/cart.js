0b572001d87068463caab91a6ee9d85d4b921a5f
curlynux <curlynux@gmail.com>
2022-07-10 18:33:17 +0200

    img tag displayed

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,33 +1,33 @@
 var data = JSON.parse(localStorage.getItem("data"));
-var img = document.getElementsByTagName("img")[5];
+var img = document.getElementsByTagName("img");
 var item = JSON.parse(localStorage.getItem("item"));
 var h2 = document.getElementsByTagName("h2")[0];
 var color = document.getElementsByTagName("p")[0];
 var prix = document.getElementsByTagName("p")[1];
 var cart = JSON.parse(localStorage.getItem("cart"));
-var couleur = JSON.parse(localStorage.getItem("color"));
-var quant = JSON.parse(localStorage.getItem("quantity"));
-var quantity = document.getElementsByClassName("itemQuantity")[0];
-fetch("http://localhost:3000/api/products")
+var color = JSON.parse(localStorage.getItem("color"));
+var section = document.getElementById("cart__items");
+var article = document.createElement("article");
+var div = document.createElement("div");
+var img5 = document.createElement("img");
+
+article.setAttribute("class", "cart__item");
+section.appendChild(article);
+div.setAttribute("class", "cart__item__img")
+div.appendChild(img5);
+article.appendChild(div);
+
+fetch(`http://localhost:3000/api/products`)
 .then(res => res.json())
 .then(data => afficherPanier(data));
-var afficherPanier = (article) => 
+var afficherPanier = (data) => 
 {
-    console.log(article);
+    console.log(data);
+    img[5].setAttribute("src", "test");
+
+    img.src = data.imageUrl;
+    h2.innerHTML = data.name;
+    color.innerHTML = data.colors[0];
+    prix.innerHTML = data.price + " €";
 }
-quantity.addEventListener("change", () => 
-{
-    console.log(quant.value)
-    localStorage.setItem("color", JSON.stringify(quant));
-});
-
-
-img.src = item.imageUrl;
-h2.innerHTML = item.name;
-prix.innerHTML = `${item.price} €`;
-color.innerHTML = couleur;
-if (quant > 1)
-    prix.innerHTML = `${item.price} €`;
-quantity.value = quant;
 console.log(cart);
-
