b6b48492ba85f5c377a7b87ba0c4f81c14a57cfc
curlynux <curlynux@gmail.com>
2022-07-15 16:03:07 +0200

    affichage conforme

--- front/js/cart.js
+++ front/js/cart.js
@@ -8,12 +8,13 @@ var cart = JSON.parse(localStorage.getItem("cart"));
 var color = JSON.parse(localStorage.getItem("color"));
 var section = document.getElementById("cart__items");
 var article = document.createElement("article");
-var div = document.createElement("div");
+var divImg = document.createElement("div");
 var img5 = document.createElement("img");
 
 var artiClone = article.cloneNode(true);
-var divClone = div.cloneNode(true);
+var divClone = divImg.cloneNode(true);
 var imgClone = img5.cloneNode(true);
+
 var divContent = document.createElement("div");
 var divDesc = document.createElement("div");
 var h2 = document.createElement("h2");
@@ -21,53 +22,57 @@ var color = document.createElement("p");
 var prix = document.createElement("p");
 var divContentClone = divContent.cloneNode(true);
 
-var divQuant = document.createElement("div");
+var divSettings = document.createElement("div");
 var divQuantChild = document.createElement("div");
 var pQuant = document.createElement("p");
 var input = document.createElement("input");
 var divDelete = document.createElement("div");
 var divDeletetext = document.createElement("p");
 
-
-div.className = "cart__item__img";
-div.appendChild(img5);
-article.appendChild(div);
-
-
 section.appendChild(article);
 article.className = "cart__item";
 artiClone.className = "cart__item";
 section.appendChild(artiClone);
 
+divImg.className = "cart__item__img";
+divImg.appendChild(img5);
+article.appendChild(divImg);
+
+
+
+
 divClone.className = "cart__item__img";
 divClone.appendChild(imgClone);
 artiClone.appendChild(divClone);
+console.log(divImg);
 
 divContent.className = "cart__item__content";
 article.appendChild(divContent);
 divDesc.className = "cart__item__content__description";
+
 divContent.appendChild(divDesc);
 divDesc.appendChild(h2);
 divDesc.appendChild(color);
 divDesc.appendChild(prix);
 
-divQuant.className = "cart__item__content__settings";
+divSettings.className = "cart__item__content__settings";
 divQuantChild.className = "cart__item__content__settings__quantity";
 input.className = "itemQuantity";
-article.appendChild(divQuant);
-divQuant.appendChild(divQuantChild);
+divContent.appendChild(divSettings);
+divSettings.appendChild(divQuantChild);
 divQuantChild.appendChild(pQuant);
 divQuantChild.appendChild(input);
+divContent.appendChild(divDelete);
 
 input.setAttribute("type", "number");
 input.name = "itemQuantity";
 input.min = 1;
 input.max = 100;
 input.value = 11;
+
 divDelete.className = "cart__item__content__settings__delete";
 divDeletetext.innerHTML = "supprimer"
 divDelete.appendChild(divDeletetext);
-article.appendChild(divDelete);
 
 
 fetch(`http://localhost:3000/api/products`)
